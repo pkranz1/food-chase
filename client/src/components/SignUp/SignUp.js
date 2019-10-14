73a3f3cb3203b0cc74ec1af677f2b62248690101
pkranz1 <pkranz346@gmail.com>
2019-10-14 18:21:52 -0400

    reorganized SignUp component

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -1,44 +1,42 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import './SignUp.css';
 
 const SignUp = ({ username, password, retypedPassword, 
   handleUsernameChange, handlePasswordChange, handleRetypedPasswordChange }) => {
     return (
-      <div>
-        <h2>SignUp</h2>
-        <form>
-          <div>
-            <p>Username</p>
-            <input
+      <div class="container justify-content-center sign-up-form">
+        <h2 class="text-center">SignUp</h2>
+        <Form>
+          <Form.Group>
+            <Form.Label>username:</Form.Label>
+            <Form.Control
               name="username"
               type="text"
+              placeholder="username"
               value={ username }
               onChange={ ({ target }) => handleUsernameChange(target.value) }
             />
-          </div>
-          <div>
-            <p>Password</p>
-            <input
+            <Form.Label>password</Form.Label>
+            <Form.Control
               name="password"
               type="password"
+              placeholder="password"
               value={ password }
               onChange={ ({ target }) => handlePasswordChange(target.value) }
             />
-          </div>
-          <div>
-            <p>ReEnter Password</p>
-            <input
-              name="retyped password"
+            <Form.Label>re-enter password</Form.Label>
+            <Form.Control
+              name="reEnter password"
               type="password"
+              placeholder="re-enter password"
               value={ retypedPassword }
               onChange={ ({ target }) => handleRetypedPasswordChange(target.value)}
             />
-          </div>
-          <div>
-            <button type="submit">SignUp</button>
-          </div>
-         </form>
+            <Button variant="primary" type="submit">Sign-up</Button>
+            </Form.Group>
+        </Form>
       </div>
     );
 }
-
 export default SignUp;
