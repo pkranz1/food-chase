fbd77d0d8a385eea03c515c24a05a6127cfae2ac
pkranz1 <pkranz346@gmail.com>
2019-11-07 17:24:25 -0500

    added restaurant user signup route

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -4,10 +4,11 @@ const { User } = require('../models');
 
 //customer signup route
 router.post('/signup/customer', (req, res, next) => {
-  const body = req.body;
+  const {email, password} = req.body;
+  
   User.create({
-    email: body.email,
-    password: body.password,
+    email,
+    password,
   })
   .then(user => {
     req.login(user, () => res.status(201).json(user));
@@ -18,9 +19,23 @@ router.post('/signup/customer', (req, res, next) => {
   });
 });
 
-router.post('signup/restaurant', (req, res, next) => {
-  
-})
+//restaurant signup route
+router.post('/signup/restaurant', (req, res, next) => {
+  const { email, password, } = req.body;
+
+  User.create({
+    email,
+    password,
+    restaurantOwner: true,
+  })
+  .then(user => {
+    req.login(user, () => res.status(201).json(user));
+  })
+  .catch(err => {
+    console.error(err);
+    next();
+  })
+});
 
 //logins in
 router.post('/login', passport.authenticate('local'), (req, res, next) => {
@@ -28,7 +43,7 @@ router.post('/login', passport.authenticate('local'), (req, res, next) => {
 });
 
 //logsout
-router.post('logout', (req, res, next) => {
+router.post('/logout', (req, res, next) => {
   req.logout();
   res.status(200).json({ message: 'Logout sucessful' });
 });
